06771d1eabb5118d5f8dc76323d945852c28dec4
Paul J.X. Tan <pjxtan@gmail.com>
2015-01-28 16:14:57 -0500

    Defined default constants; defined some getters; changed access specifiers

--- java/odeint/Lorenz.java
+++ java/odeint/Lorenz.java
@@ -1,3 +1,5 @@
+package odeint;
+
 /**
  * The Lorenz system. The equations are:
  *
@@ -5,12 +7,21 @@
  * dy/dt = x * (rho - z) - y
  * dz/dt = x * y - beta * z
  */
-public class Lorenz implements Eom {
+class Lorenz implements Eom {
     /** The number of dimensions in this system. */
-    public final int N_DIMS = 3;
+    private static final int N_DIMS = 3;
 
     /** The number of constants in the system of ODEs. */
-    public final int N_CONSTS = 3;
+    private static final int N_CONSTS = 3;
+
+    /** The default value for the constant sigma. */
+    private static final double SIGMA_DEFAULT = 10.0;
+    
+    /** The default value for the constant rho. */
+    private static final double RHO_DEFAULT = 28.0;
+    
+    /** The default value for the constant sigma. */
+    private static final double BETA_DEFAULT = 8.0 / 3.0;
 
     /** The constant sigma. */
     private final double SIGMA;
@@ -22,20 +33,27 @@ public class Lorenz implements Eom {
     private final double BETA;
 
     /** A constructor for using default constants. */
-    public Lorenz() {
-        SIGMA = 10.0;
-        RHO = 28.0;
-        BETA = 8.0 / 3.0;
+    Lorenz() {
+        SIGMA = SIGMA_DEFAULT;
+        RHO = RHO_DEFAULT;
+        BETA = BETA_DEFAULT;
     }
     
     /** A constructor for using specified constants. */
-    public Lorenz(double[] C) {
+    Lorenz(double[] C) {
         SIGMA = C[0];
         RHO = C[1];
         BETA = C[2];
     }
 
-    @Override
+    public int getNDims() {
+        return N_DIMS;
+    }
+
+    public int getNConsts() {
+        return N_CONSTS;
+    }
+
     public double[] evaluate(double t, double[] X) {
         double x = X[0], y = X[1], z = X[2];
 
