7aec9a8648342f7339d3402e715cd4f8447ac199
Paul J.X. Tan <pjxtan@gmail.com>
2015-01-28 16:13:47 -0500

    Declared methods for returning number of dimensions and constants; changed access specifiers

--- java/odeint/Eom.java
+++ java/odeint/Eom.java
@@ -1,10 +1,16 @@
+package odeint;
+
 /**
  * Describes any class containing equations of motion (i.e. a system of ODEs)
  * as input for the integrator.
  */
-public interface Eom {
-    /**
-     * Evaluates the system of ODEs.
-     */
+interface Eom {
+    /** Returns the number of dimensions in the system. */
+    int getNDims();
+
+    /** Returns the number of constants in the system. */
+    int getNConsts();
+
+    /** Evaluates the system of ODEs. */
     double[] evaluate(double t, double[] X);
 }
