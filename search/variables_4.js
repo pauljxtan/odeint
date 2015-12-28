260b889ced15474c8726bf6f36e719e9c8175278
Paul J.X. Tan <pjxtan@gmail.com>
2015-12-28 16:24:49 -0500

    Updated

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['n_5fsteps',['n_steps',['../structbulsto.html#ab0f98110059509f36682188917a94e82',1,'bulsto::n_steps()'],['../structfweuler.html#a3ed5377d7609cbeef32422ad5662ab62',1,'fweuler::n_steps()'],['../structrk4.html#afdb98234387428a832f96e7ccc2ec470',1,'rk4::n_steps()']]],
-  ['n_5fvars',['n_vars',['../structbulsto.html#a4644ac965956cf8a34a102aa42882fa9',1,'bulsto::n_vars()'],['../structfunction__table.html#ae4e3f9023fbce759fc423e921e51d43f',1,'function_table::n_vars()'],['../structfweuler.html#a79f8c4cfdb9adfa253c8b4c037c237f8',1,'fweuler::n_vars()'],['../structrk4.html#a1771335b8075b6c2bc2f7f365ece307b',1,'rk4::n_vars()']]],
+  ['n_5fvars',['n_vars',['../structfunction__table.html#ae4e3f9023fbce759fc423e921e51d43f',1,'function_table']]],
   ['name',['name',['../structfunction__table.html#a99650448c98f34476adf6caf60a22df2',1,'function_table']]]
 ];
